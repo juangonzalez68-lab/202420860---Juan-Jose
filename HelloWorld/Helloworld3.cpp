5b59bf5a5609180ad7c94a8501040c184038308f
juangonzalez68-lab <juan.gonzalez68@uptc.edu.co>
2025-08-21 16:47:08 +0000

    uso de User

---
+++ HelloWorld/Helloworld3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string userName = "Juan";
+    int userAge = 19;
+    cout << "Hola Mundo!" << userName << endl ;
+    cout << "tu edad es " << userAge << endl ;
+    return 0;
+}
