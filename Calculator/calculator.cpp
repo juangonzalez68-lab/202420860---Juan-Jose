0735c340983c4f5e23d215dc60a566dadb3a5620
juangonzalez68-lab <juan.gonzalez68@uptc.edu.co>
2025-08-26 20:11:59 +0000

    calculadora

---
+++ Calculator/calculator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    string  variable1, variable2;
+    int multiplication, division, modulo, suma, substrac;
+    // suma = 1 // substrac = 2 // multiplication = 3 // division = 4 // module
+    
+    cout << 
+     
+     
+
+
+    return 0;
+    }
+
---
+++ Calculator/calculator2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double num1, num2, resultado;
+    char operacion;
+
+    cout << "Calculadora basica" << endl;
+    cout << "Operaciones disponibles: +  -  *  /" << endl;
+
+    cout << "Ingresa el primer numero: ";
+    cin >> num1;
+
+    cout << "Ingresa el segundo numero: ";
+    cin >> num2;
+
+    cout << "Elige la operacion (+, -, *, /): ";
+    cin >> operacion;
+
+    if (operacion == '+') {
+        resultado = num1 + num2;
+        cout << "Resultado: " << resultado << endl;
+    }
+    else if (operacion == '-') {
+        resultado = num1 - num2;
+        cout << "Resultado: " << resultado << endl;
+    }
+    else if (operacion == '*') {
+        resultado = num1 * num2;
+        cout << "Resultado: " << resultado << endl;
+    }
+    else if (operacion == '/') {
+        if (num2 != 0) {
+            resultado = num1 / num2;
+            cout << "Resultado: " << resultado << endl;
+        } else {
+            cout << "Error: No se puede dividir entre cero." << endl;
+        }
+    }
+    else {
+        cout << "Operacion no valida." << endl;
+    }
+
+    return 0;
+}
