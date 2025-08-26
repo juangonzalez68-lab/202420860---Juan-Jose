619f1b868f94005a4a5fb4d7b05b104008cafe34
juangonzalez68-lab <juan.gonzalez68@uptc.edu.co>
2025-08-26 20:41:12 +0000

    calculadora

--- Calculator/calculator.cpp
+++ Calculator/calculator.cpp
@@ -3,14 +3,31 @@
 using namespace std;
 
 int main () {
-    string  variable1, variable2;
+    int  variable1, variable2;
+
     int multiplication, division, modulo, suma, substrac;
     // suma = 1 // substrac = 2 // multiplication = 3 // division = 4 // module
     
-    cout << 
-     
-     
+    cout << "ingrese el primer digito";
+    cin >> variable1;
+    cout << "ingrese el segundo digito";
+    cin >> variable2;
 
+    cout << "suma:";
+    suma = variable1 + variable2 ;
+    cout << suma << endl; 
+    cout << "resta:";
+    substrac = variable1 - variable2 ;
+    cout << substrac << endl;
+    cout << "multiplicacion: ";
+    multiplication = variable1 * variable2;
+    cout << multiplication << endl;
+    cout << "division:" ;
+    division = variable1 / variable2;
+    cout << division << endl;
+    modulo = variable1 % variable2;
+    cout << "modulo:" ;
+    cout << modulo << endl;
 
     return 0;
     }
