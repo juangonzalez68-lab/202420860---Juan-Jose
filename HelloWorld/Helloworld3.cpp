ca009beec579c0bf6d15d1d8e9c1f6a27285e6e8
juangonzalez68-lab <juan.gonzalez68@uptc.edu.co>
2025-08-26 16:17:28 +0000

    Temperatura

--- HelloWorld/Helloworld3.cpp
+++ HelloWorld/Helloworld3.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     string userName = "Juan";
     int userAge = 19;
-    cout << "Hola Mundo!" << userName << endl ;
+    cout << "Hola Mundo,soy " << userName << endl ;
     cout << "tu edad es " << userAge << endl ;
     return 0;
-}
+} 
