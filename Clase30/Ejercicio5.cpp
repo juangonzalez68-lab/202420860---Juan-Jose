6c54faa3fca856fc6d84bbf6dc599c79b7e6ae23
juangonzalez68-lab <juan.gonzalez68@uptc.edu.co>
2025-10-30 16:57:38 +0000

    PolloAsado2

--- Clase30/Ejercicio5.cpp
+++ Clase30/Ejercicio5.cpp
@@ -4,12 +4,14 @@ using namespace std;
 void forWithArray()
 {
     string products[3] = {"Pan", "Leche", "Queso"}; 
-    float prices[3] = {1500, 3200, 4800};;
+    float prices[3] = {1500, 3200, 4800};
     
    for (int i = 0 ; i < 3 ;i++)
         {
         string arrayValue = products[i];
-        cout << "los productos son: " << arrayValue << endl;
+        float arrayPrice = prices[i];
+        cout << "El producto: " << arrayValue << endl;
+        cout << "Tiene un precio de: " << arrayPrice << endl;
         }
 }
 
